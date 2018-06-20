6df683bf13de0da126a35c8b78cd185f972b97df
nathan-08 <nathanryan001@gmail.com>
2018-06-20 11:55:37 -0600

    cleanup

--- src/index.js
+++ src/index.js
@@ -5,19 +5,7 @@ import "./main.sass";
 const Index = () => {
   return (
     <div>
-      <blockquote>
-        ipse Epicurus obit decurso lumine vitae,<br />
-        qui genus humanum ingenio superavit et omnis<br />
-        restinxit stellas exortus ut aetherius sol.
-      </blockquote>
-      <blockquote>
-        Lucretius, <em>De Rerum Natura</em>
-      </blockquote>
-      <blockquote>
-        Epicurus himself, the light of life expiring, died,<br />
-        Who had in mind the human kind excell'd,<br />
-        As the risen sun all heaven's stars extinguish'd.
-      </blockquote>
+      <blockquote>Webpack react setup</blockquote>
     </div>
   );
 };
